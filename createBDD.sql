8e642eb4a8f2cec6865ed396d00ee2d04034dad8
Pierre-Henri Colonel <pcolonel@4as.insa-cvl.fr>
2018-02-03 18:52:52 +0100

    test

--- createBDD.sql
+++ createBDD.sql
@@ -6,3 +6,13 @@ CREATE TABLE `markers` (
   `lng` FLOAT( 10, 6 ) NOT NULL ,
   `type` VARCHAR( 30 )
 ) ENGINE = MYISAM ;
+
+CREATE TABLE `users` (
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  `name` VARCHAR( 60 ) NOT NULL ,
+  `firstname` VARCHAR( 60 ) NOT NULL ,
+  `address` VARCHAR( 80 ) NOT NULL ,
+  `phone` INT( 10 ) NOT NULL ,
+  `email` VARCHAR( 60 ) NOT NULL ,
+  `age` INT( 2 )
+) ENGINE = MYISAM ;
--- index.php
+++ index.php
@@ -1,117 +1,205 @@
-<!DOCTYPE html >
-  <head>
-    <meta name="description" content="DébarraVite, plateforme collaborative, met en relation des particuliers afin de faciliter le recyclage d'encombrants.">
+<?php	
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- Theme Made By www.w3schools.com - No Copyright -->
+  <meta name="description" content="DébarraVite, plateforme collaborative, met en relation des particuliers afin de faciliter le recyclage d'encombrants.">
+  <title>Bootstrap Theme Simply Me</title>
+  <meta charset="utf-8">
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
-    <title>DebarraVite</title>
-    <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="lightbox.js" type="text/javascript"></script>
+  <style>
+  #map {
         height: 100%;
       }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
+  html, body {
         height: 100%;
         margin: 0;
         padding: 0;
       }
-    </style>
-	
-	<?php 
+  body {
+      font: 20px Montserrat, sans-serif;
+      line-height: 1.8;
+      color: #f5f6f7;
+  }
+  p {font-size: 16px;}
+  .margin {margin-bottom: 45px;}
+  .bg-1 { 
+      background-color: #1abc9c; /* Green */
+      color: #ffffff;
+  }
+  .bg-2 { 
+      background-color: #474e5d; /* Dark Blue */
+      color: #ffffff;
+  }
+  .bg-3 { 
+      background-color: #ffffff; /* White */
+      color: #555555;
+  }
+  .bg-4 { 
+      background-color: #2f2f2f; /* Black Gray */
+      color: #fff;
+  }
+  .container-fluid {
+      padding-top: 70px;
+      padding-bottom: 70px;
+  }
+  .navbar {
+      padding-top: 15px;
+      padding-bottom: 15px;
+      border: 0;
+      border-radius: 0;
+      margin-bottom: 0;
+      font-size: 12px;
+      letter-spacing: 5px;
+  }
+  .navbar-nav  li a:hover {
+      color: #1abc9c !important;
+  }
+  .button_connexion {
+    background-color: #1400dd;
+    line-height: 2;
+    color: #FFFFFF;
+    margin-top: 3%;
+    border-radius: 8px;
+    padding: 0 1.5%;
+}
+.button_inscription {
+    background-color: #646600;
+    line-height: 2;
+    color: #FFFFFF;
+    margin-top: 3%;
+    border-radius: 8px;
+    padding: 0 1.5%;
+}
+#box {
+    display: none;
+    position: fixed;
+    top: 10%;
+    left: 25%;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px solid black;
+    background-color: #C9C9C9;
+    z-index: 101;
+    overflow: auto;
+    line-height: 1.5;
+    padding: 5%;
+}
+#boxtitle {
+    position: absolute;
+    float: center;
+    top: 0;
+    left: 0;
+    width: 100%;
+    padding: 0;
+        padding-top: 0px;
+    padding-top: 4px;
+    left-padding: 8px;
+    margin: 0;
+    border-bottom: 4px solid #448156;
+    background-color: #448156;
+    color: white;
+    text-align: center;
+    font-size: 25px;
+}
+#shadowing {
+    display: none;
+    position: fixed;
+    top: 0%;
+    left: 0%;
+    width: 100%;
+    height: 100%;
+    background-color: #CCA;
+    z-index: 10;
+    opacity: 0.5;
+    filter: alpha(opacity=50);
+}
+  </style>
+  
+  <?php 
 		include("xmlCreate.php"); 
 	?>
-	
-  </head>
 
-  <body>
-	<a href="ajout.php">
-		<i class="fa fa-bed nav_icon"></i>
-		<span class="nav_label">Je dépose mon annonce</span>
-	</a>
-    <div id="map"></div>
+</head>
+<body>
 
-    <script>
-      var customLabel = {
-        Déchèterie: {
-          label: 'D'
-        },
-        bar: {
-          label: 'B'
-        }
-      };
+<!-- Navbar -->
+<nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">DébarraVite</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+		<ul><input onclick="openbox('Connexion', 1)" value="Connexion" class="button_connexion" type="button"></ul>
+      </ul>
+    </div>
+  </div>
+</nav>
 
-        function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: new google.maps.LatLng(47.082513, 2.399103),
-          zoom: 12
-        });
-        var infoWindow = new google.maps.InfoWindow;
+<div id="shadowing"></div>
+	<div id="box">
+		<span id="boxtitle"></span>	
+		<?php
+			include("register.php");
+		?>
+	</div>
+</div>
 
-          // Change this depending on the name of your PHP or XML file
-          downloadUrl('markers.xml', function(data) {
-            var xml = data.responseXML;
-			console.log(markers);
-            var markers = xml.documentElement.getElementsByTagName('marker');
-            Array.prototype.forEach.call(markers, function(markerElem) {
-              var name = markerElem.getAttribute('name');
-              var address = markerElem.getAttribute('address');
-              var type = markerElem.getAttribute('type');
-              var point = new google.maps.LatLng(
-                  parseFloat(markerElem.getAttribute('lat')),
-                  parseFloat(markerElem.getAttribute('lng')));
+<!-- First Container -->
+<div class="container-fluid bg-1 text-center">
+  <h3 class="margin">Bienvenue sur DébarraVite</h3>
+  <h3>La plateforme collaborative qui met en relation des particuliers afin de faciliter le recyclage d'encombrants. </h3>
+  <iframe width="860" height="480" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
+  <br>
+  <input onclick="openbox('Inscription', 1)" value="Inscription" class="button_inscription" type="button">
+</div>
 
-              var infowincontent = document.createElement('div');
-              var strong = document.createElement('strong');
-              strong.textContent = name
-              infowincontent.appendChild(strong);
-              infowincontent.appendChild(document.createElement('br'));
-			  
-			  var strong = document.createElement('strong');
-              strong.textContent = type
-              infowincontent.appendChild(strong);
-              infowincontent.appendChild(document.createElement('br'));
+<!-- Second Container -->
+<div class="container-fluid bg-2 text-center">
+  <h3 class="margin">What Am I?</h3>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+  <a href="#" class="btn btn-default btn-lg">
+    <span class="glyphicon glyphicon-search"></span> Search
+  </a>
+</div>
 
-              var text = document.createElement('text');
-              text.textContent = address
-              infowincontent.appendChild(text);
-              var icon = customLabel[type] || {};
-              var marker = new google.maps.Marker({
-                map: map,
-                position: point,
-                label: icon.label
-              });
-              marker.addListener('click', function() {
-                infoWindow.setContent(infowincontent);
-                infoWindow.open(map, marker);
-              });
-            });
-          });
-        }
+<!-- Third Container (Grid) -->
+<div class="container-fluid bg-3 text-center">    
+  <h3 class="margin">Where To Find Me?</h3><br>
+  <div class="row">
+    <div class="col-sm-4">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <img src="birds1.jpg" class="img-responsive margin" style="width:100%" alt="Image">
+    </div>
+    <div class="col-sm-4"> 
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <img src="birds2.jpg" class="img-responsive margin" style="width:100%" alt="Image">
+    </div>
+    <div class="col-sm-4"> 
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      <img src="birds3.jpg" class="img-responsive margin" style="width:100%" alt="Image">
+    </div>
+  </div>
+</div>
 
+<!-- Footer -->
+<footer class="container-fluid bg-4 text-center">
+  <p>Bootstrap Theme Made By <a href="https://www.w3schools.com">www.w3schools.com</a></p> 
+</footer>
 
-
-      function downloadUrl(url, callback) {
-        var request = window.ActiveXObject ?
-            new ActiveXObject('Microsoft.XMLHTTP') :
-            new XMLHttpRequest;
-
-        request.onreadystatechange = function() {
-          if (request.readyState == 4) {
-            request.onreadystatechange = doNothing;
-            callback(request, request.status);
-          }
-        };
-
-        request.open('GET', url, true);
-        request.send(null);
-      }
-
-      function doNothing() {}
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAA5eSkmcNDH8nw7wGcgNSF83GdN2d9PMU&libraries=places&callback=initMap">
-    </script>
-  </body>
+</body>
 </html>
-	
